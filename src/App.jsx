f32b7e0e6a1c940054d89b7b7ba12c5a0e1f0f49
yar-24 <akhyar24.354@gmail.com>
2023-09-21 13:03:41 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,36 @@
+import { BrowserRouter } from 'react-router-dom';
+import {
+  About,
+  Contact,
+  Experience,
+  Certificates,
+  Hero,
+  Navbar,
+  Tech,
+  StarsCanvas,
+  Works,
+} from './components';
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <div className="relative z-0 bg-primary">
+        <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
+          <Navbar />
+          <Hero />
+        </div>
+        <About />
+        <Experience />
+        <Tech />
+        <Works />
+        <Certificates />
+        <div className="relative z-0">
+          <Contact />
+          <StarsCanvas />
+        </div>
+      </div>
+    </BrowserRouter>
+  );
+};
+
+export default App;
---
+++ src/constants/index.js
@@ -0,0 +1,314 @@
+import {
+  mobile,
+  backend,
+  creator,
+  web,
+  javascript,
+  typescript,
+  html,
+  css,
+  reactjs,
+  redux,
+  tailwind,
+  nodejs,
+  mongodb,
+  git,
+  figma,
+  docker,
+  dicoding,
+  bintang,
+  gundar,
+  nafas,
+  eundang,
+  bintangphoto,
+  threejs,
+} from '../assets';
+
+export const navLinks = [
+  {
+    id: 'about',
+    title: 'About',
+  },
+  {
+    id: 'work',
+    title: 'Work',
+  },
+  {
+    id: 'contact',
+    title: 'Contact',
+  },
+];
+
+const services = [
+  {
+    title: 'Frontend Developer',
+    icon: web,
+  },
+  {
+    title: 'Backend Developer',
+    icon: backend,
+  },
+  {
+    title: 'React Native Developer',
+    icon: mobile,
+  },
+  {
+    title: 'Fullstack Developer',
+    icon: creator,
+  },
+];
+
+const technologies = [
+  {
+    name: 'HTML 5',
+    icon: html,
+  },
+  {
+    name: 'CSS 3',
+    icon: css,
+  },
+  {
+    name: 'JavaScript',
+    icon: javascript,
+  },
+  {
+    name: 'TypeScript',
+    icon: typescript,
+  },
+  {
+    name: 'React JS',
+    icon: reactjs,
+  },
+  {
+    name: 'Redux Toolkit',
+    icon: redux,
+  },
+  {
+    name: 'Tailwind CSS',
+    icon: tailwind,
+  },
+  {
+    name: 'Node JS',
+    icon: nodejs,
+  },
+  {
+    name: 'MongoDB',
+    icon: mongodb,
+  },
+  {
+    name: 'Three JS',
+    icon: threejs,
+  },
+  {
+    name: 'git',
+    icon: git,
+  },
+  {
+    name: 'figma',
+    icon: figma,
+  },
+  {
+    name: 'docker',
+    icon: docker,
+  },
+];
+
+const experiences = [
+  {
+    title: 'Front-End Web and React Developer',
+    company_name: 'Study Independent | Dicoding',
+    icon: dicoding,
+    iconBg: '#E6DEDD',
+    date: 'Aug 2022 - Des 2022',
+    points: [
+      'Dicoding Learned and applied a wide range of web front-end skills, from basic to expert level, while collaborating with a team.',
+      'As a team leader, I was responsible for generating ideas, developing both frontend and backend components, and presenting projects.',
+    ],
+  },
+  {
+    title: 'Full-Stack Web Developer',
+    company_name: 'Scientific Writing',
+    icon: gundar,
+    iconBg: '#383E56',
+    date: 'Apr 2022 - Aud 2022',
+    points: [
+      'Universitas Gunadarma Developed an online invitation web application using the MERN stack technology.',
+      'Implemented a payment gateway from midtrans to facilitate secure and seamless transactions within the application.',
+      'Implementing responsive design and ensuring cross-browser compatibility.',
+    ],
+  },
+  {
+    title: 'Part-time Front-End Developer',
+    company_name: 'Bintang Studio',
+    icon: bintang,
+    iconBg: '#E6DEDD',
+    date: '2021-2022',
+    points: [
+      'Developing and maintaining web applications using React.js and other related technologies.',
+      'Created a visually appealing photography service website using ReactJS.',
+      'Implementing responsive design and ensuring cross-browser compatibility.',
+    ],
+  },
+];
+
+const certificates = [
+  {
+    title: 'Weekly Class REACT.JS WEB DEVELOPMENT',
+    name: 'Google Developer Student Clubs',
+    link: 'https://ibb.co/54s1VtD',
+    image:
+      'https://studentorgs.engineering.asu.edu/wp-content/uploads/sites/35/2023/01/GDSC-Crop.png',
+  },
+  {
+    title: 'Weekly Class Web Development 2022',
+    name: 'Google Developer Student Clubs',
+    link: 'https://ibb.co/gSz7PYh',
+    image:
+      'https://studentorgs.engineering.asu.edu/wp-content/uploads/sites/35/2023/01/GDSC-Crop.png',
+  },
+  {
+    title: 'Weekly Class UI/UX Design 2022',
+    name: 'Google Developer Student Clubs',
+    link: 'https://ibb.co/gw1Lsbj',
+    image:
+      'https://studentorgs.engineering.asu.edu/wp-content/uploads/sites/35/2023/01/GDSC-Crop.png',
+  },
+  {
+    title: 'Weekly Class Android 2022',
+    name: 'Google Developer Student Clubs',
+    link: 'https://ibb.co/qjjCC2j',
+    image:
+      'https://studentorgs.engineering.asu.edu/wp-content/uploads/sites/35/2023/01/GDSC-Crop.png',
+  },
+  {
+    title: 'Memulai Dasar Pemrograman untuk Menjadi Pengembang Software',
+    name: 'Dicoding Indonesia',
+    link: 'https://www.dicoding.com/certificates/JMZVG8GDJZN9',
+    image:
+      'https://dicoding-web-img.sgp1.cdn.digitaloceanspaces.com/original/jobs/dos:lowongan_dicoding_dicoding_indonesia_010322094804.png',
+  },
+  {
+    title: 'Belajar Dasar Pemrograman Web',
+    name: 'Dicoding Indonesia',
+    link: 'https://www.dicoding.com/certificates/2VX31765JZYQ',
+    image:
+      'https://dicoding-web-img.sgp1.cdn.digitaloceanspaces.com/original/jobs/dos:lowongan_dicoding_dicoding_indonesia_010322094804.png',
+  },
+  {
+    title: 'Belajar Dasar Git dengan GitHub',
+    name: 'Dicoding Indonesia',
+    link: 'https://www.dicoding.com/certificates/6RPND01Y5Z2M',
+    image:
+      'https://dicoding-web-img.sgp1.cdn.digitaloceanspaces.com/original/jobs/dos:lowongan_dicoding_dicoding_indonesia_010322094804.png',
+  },
+  {
+    title: 'Belajar Membuat Front-End Web untuk Pemula',
+    name: 'Dicoding Indonesia',
+    link: 'https://www.dicoding.com/certificates/L4PQ40MQOXO1',
+    image:
+      'https://dicoding-web-img.sgp1.cdn.digitaloceanspaces.com/original/jobs/dos:lowongan_dicoding_dicoding_indonesia_010322094804.png',
+  },
+  {
+    title: 'Belajar Fundamental Front-End Web Development',
+    name: 'Dicoding Indonesia',
+    link: 'https://www.dicoding.com/certificates/07Z6RO8M2PQR',
+    image:
+      'https://dicoding-web-img.sgp1.cdn.digitaloceanspaces.com/original/jobs/dos:lowongan_dicoding_dicoding_indonesia_010322094804.png',
+  },
+  {
+    title: 'Belajar Membuat Aplikasi Web dengan React',
+    name: 'Dicoding Indonesia',
+    link: 'https://www.dicoding.com/certificates/6RPNDM399Z2M',
+    image:
+      'https://dicoding-web-img.sgp1.cdn.digitaloceanspaces.com/original/jobs/dos:lowongan_dicoding_dicoding_indonesia_010322094804.png',
+  },
+  {
+    title: 'Belajar Fundamental Aplikasi Web dengan React',
+    name: 'Dicoding Indonesia',
+    link: 'https://www.dicoding.com/certificates/L4PQ6M6MVPO1',
+    image:
+      'https://dicoding-web-img.sgp1.cdn.digitaloceanspaces.com/original/jobs/dos:lowongan_dicoding_dicoding_indonesia_010322094804.png',
+  },
+  {
+    title: 'Menjadi Front-End Web Developer Expert',
+    name: 'Dicoding Indonesia',
+    link: 'https://www.dicoding.com/certificates/6RPN643G5P2M',
+    image:
+      'https://dicoding-web-img.sgp1.cdn.digitaloceanspaces.com/original/jobs/dos:lowongan_dicoding_dicoding_indonesia_010322094804.png',
+  },
+  {
+    title: 'Menjadi React Web Developer Expert',
+    name: 'Dicoding Indonesia',
+    link: 'https://www.dicoding.com/certificates/JMZVN792OPN9',
+    image:
+      'https://dicoding-web-img.sgp1.cdn.digitaloceanspaces.com/original/jobs/dos:lowongan_dicoding_dicoding_indonesia_010322094804.png',
+  },
+];
+
+const projects = [
+  {
+    name: 'Nafas',
+    description:
+      'Web-based platform that allows users to search, buy, and manage blog, providing a convenient and efficient solution for plants your needs.',
+    tags: [
+      {
+        name: 'MERN Stack',
+        color: 'blue-text-gradient',
+      },
+      {
+        name: 'Javascript ES6',
+        color: 'green-text-gradient',
+      },
+      {
+        name: 'material ui',
+        color: 'pink-text-gradient',
+      },
+    ],
+    image: nafas,
+    source_code_link: 'https://nafas-mu.vercel.app/',
+  },
+  {
+    name: 'EUndang',
+    description:
+      'A web application that allows users to order invitation templates, view interesting themes, and be able to order with a paymentgateway.',
+    tags: [
+      {
+        name: 'react',
+        color: 'blue-text-gradient',
+      },
+      {
+        name: 'restapi',
+        color: 'green-text-gradient',
+      },
+      {
+        name: 'scss',
+        color: 'pink-text-gradient',
+      },
+    ],
+    image: eundang,
+    source_code_link: 'https://github.com/yar-24/admin-eundang',
+  },
+  {
+    name: 'Bintang photo',
+    description:
+      'Ordering and service platform available at the star photo studio company.',
+    tags: [
+      {
+        name: 'reactjs',
+        color: 'blue-text-gradient',
+      },
+      {
+        name: 'responsive',
+        color: 'green-text-gradient',
+      },
+      {
+        name: 'css',
+        color: 'pink-text-gradient',
+      },
+    ],
+    image: bintangphoto,
+    source_code_link: 'https://github.com/yar-24/bintangphoto',
+  },
+];
+
+export { services, technologies, experiences, certificates, projects };
